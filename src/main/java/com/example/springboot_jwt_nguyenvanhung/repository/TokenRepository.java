e6b91454cde03da98b63faa4d6551a07164574dd
HungNg2k <chadkeeper2809@gmail.com>
2021-09-27 11:52:00 +0700

    commit filter-testPermission

--- src/main/java/com/example/springboot_jwt_nguyenvanhung/repository/TokenRepository.java
+++ src/main/java/com/example/springboot_jwt_nguyenvanhung/repository/TokenRepository.java
@@ -3,5 +3,6 @@ package com.example.springboot_jwt_nguyenvanhung.repository;
 import com.example.springboot_jwt_nguyenvanhung.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TokenRepository extends JpaRepository<Token,Long> {
+public interface TokenRepository extends JpaRepository<Token, Long> {
+    Token findByToken(String token);
 }
