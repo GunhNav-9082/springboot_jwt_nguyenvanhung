bc4df006d169bc3f66780e18b3a1fa49e491ba3d
HungNg2k <chadkeeper2809@gmail.com>
2021-09-27 10:55:35 +0700

    Initial commit

---
+++ src/main/java/com/example/springboot_jwt_nguyenvanhung/SpringbootJwtNguyenvanhungApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_jwt_nguyenvanhung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtNguyenvanhungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJwtNguyenvanhungApplication.class, args);
+    }
+
+}
